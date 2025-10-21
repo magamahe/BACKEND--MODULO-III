a04497595065fb4d2e010cd5eec3861494b4d166
Magamahe <magamahe@gmail.com>
2025-10-21 10:27:51 -0300

    Dejo de trackear la carpeta dist

--- dist/clase_01/ejercicios/ejercicio_02.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let variable = 10; // inicializamos con un número
-console.log("Valor inicial (número):", variable);
-variable = "Ahora soy un texto"; // cambiamos a string
-console.log("Valor modificado (texto):", variable);
-//# sourceMappingURL=ejercicio_02.js.map
